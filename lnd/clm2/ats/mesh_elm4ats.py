4abc303ebe12f2f4a755d9561b14f805533f93d7
Ethan Coon <coonet@ornl.gov>
2022-02-04 09:50:47 -0500

    updates meshing process to create a mesh in which cell 0 is the top, cell i is the ith from the top, adds DayMet data for nearby to Fengming location (except on land, not in the lake itself), updates xml to running with basic precip from DayMet

--- lnd/clm2/ats/mesh_elm4ats.py
+++ lnd/clm2/ats/mesh_elm4ats.py
@@ -1,7 +1,4 @@
-"""
-Example of using meshing_ats.py to generate a column mesh with layers and variable resolution.
-
-"""
+"""Generate ATS ExodusII mesh that is identical to an ELM column"""
 
 import sys,os
 import numpy as np
@@ -11,13 +8,17 @@ from copy import deepcopy
 try:
     import meshing_ats
 except ImportError:
-    #sys.path.append(os.path.join(os.environ['ATS_SRC_DIR'],'tools','meshing', 'meshing_ats'))
-    sys.path.append(os.path.join('/Users/f9y/mygithub/ATS_REPOS/amanzi/src/physics/ats', \
-                                 'tools','meshing', 'meshing_ats'))
-    import meshing_ats
+    try:
+        sys.path.append(os.path.join(os.environ['ATS_SRC_DIR'],'tools','meshing', 'meshing_ats'))
+        import meshing_ats
+    except ImportError:
+        sys.path.append(os.path.join('/Users/f9y/mygithub/ATS_REPOS/amanzi/src/physics/ats', \
+                                     'tools','meshing', 'meshing_ats'))
+        import meshing_ats
 
 # set up the surface mesh, which is a single unit cell
 # TODO - read-in latixy/longxy/topo from 'surfdata.nc' and do some lat/lon<->x/y conversion
+#    - ETC: this can be done with WW straightforwardly
 x = np.array([0.0, 1.0],'d')
 elv = np.array([0.0, 0.0], 'd')
 
@@ -37,7 +38,6 @@ z = 0.0
 #  variable layer thickness
 #  15 layers
 #  mat-id for each top 10 layer and 1 for rest 5 layers (called bedrock in ELM)
-
 ncells = 15
 jidx = np.array(range(ncells))+1 
 zsoi = 0.025*(np.exp(0.5*(jidx-0.5))-1.0)       #ELM soil layer node depths - somewhere inside a layer but not centroid
@@ -49,24 +49,19 @@ dzsoi[ncells-1] = zsoi[ncells-1]-zsoi[ncells-2]
 
 nlevsoi = 10
 for j in range(ncells):
-    i=ncells-j-1
-    z = z+dzsoi[i]
-    print('z, dz, z_centroid, z_node_elm: ', i, z, dzsoi[i], z-dzsoi[i]/2.0, zsoi[i])
-    layer_types.append("snapped")
-    layer_data.append(z)
+    z = z - dzsoi[j]
+    print('j, z, dz, z_centroid, z_node_elm: ', j, z, dzsoi[j], z+dzsoi[j]/2.0, -zsoi[j])
+    layer_types.append("constant")
+    layer_data.append(dzsoi[j])
     layer_ncells.append(1)
-    if i<nlevsoi:
-        layer_mat_ids.append(1001+i)
+    if j<nlevsoi:
+        layer_mat_ids.append(1001+j)
     else:
         layer_mat_ids.append(1001+nlevsoi)
 
 
 # -- print out a summary --
-#meshing_ats.summarize_extrusion(layer_types, layer_data, layer_ncells, layer_mat_ids)
-dz = deepcopy(layer_data)
-dz.insert(0,0.0)
-dz = np.diff(dz)
-meshing_ats.summarize_extrusion(layer_types, dz, layer_ncells, layer_mat_ids)   # here 'layer_data' shall be 'dz'
+meshing_ats.summarize_extrusion(layer_types, layer_data, layer_ncells, layer_mat_ids)
 
 # Extrude the 3D model with this structure and write to file
 m3 = meshing_ats.Mesh3D.extruded_Mesh2D(m2, layer_types, 
