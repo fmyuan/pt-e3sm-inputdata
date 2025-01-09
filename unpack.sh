0a56920362cfa4c8bc594a0a99a1a737e86a79c1
Ethan Coon <coonet@ornl.gov>
2025-01-08 20:54:55 -0700

    Add unpack script.

---
+++ unpack.sh
@@ -0,0 +1,5 @@
+cd lnd/clm2/firedata
+tar -zxvf clmforc.Li_2012_hdm_0.5x0.5_AVHRR_simyr1850-2010_c130401.nc.tar.gz
+cd ../../../atm/datm7/NASA_LIS/
+tar -zxvf clmforc.Li_2012_climo1995-2011.T62.lnfm_c130327.nc.tar.gz
+tar -zxvf clmforc.Li_2012_climo1995-2011.T62.lnfm_Total_c140423.nc.tar.gz
