23fa69741afed41c8f35f61c39f6020bc26c98dc
Fengming Yuan <fmyuan@gmail.com>
2025-04-24 15:45:31 -0400

    add paramter file for testing TAI code development.

--- unpack.sh
+++ unpack.sh
@@ -4,3 +4,8 @@ tar -xzf elmforc.ssp5_hdm_0.5x0.5_simyr1850-2100_c190109.tar.gz
 cd ../../../atm/datm7/NASA_LIS/
 tar -zxvf clmforc.Li_2012_climo1995-2011.T62.lnfm_c130327.nc.tar.gz
 tar -zxvf clmforc.Li_2012_climo1995-2011.T62.lnfm_Total_c140423.nc.tar.gz
+cd ../
+tar -zxvf atm_forcing.datm7.Qian.T62.c080727_smallvilleIA.tar.gz
+ln -sf atm_forcing.datm7.Qian.T62.c080727_smallvilleIA atm_forcing.datm7.Qian.T62.c080727
+ln -sf domain.T62.050609_smallvilleIA.nc domain.T62.050609.nc 
+
