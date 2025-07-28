4ef28390c7e05bedc96b14f4509a89a9d56c65a7
Fengming Yuan <fmyuan@gmail.com>
2025-07-28 11:27:57 -0400

    Add a new fire module required input data.

--- unpack.sh
+++ unpack.sh
@@ -1,6 +1,7 @@
 cd lnd/clm2/firedata
 tar -zxvf clmforc.Li_2012_hdm_0.5x0.5_AVHRR_simyr1850-2010_c130401.nc.tar.gz
 tar -xzf elmforc.ssp5_hdm_0.5x0.5_simyr1850-2100_c190109.tar.gz
+tar -xzf elmforc.Li_20181205_mod_hist_SSP2_CMIP6_hdm_0.5x0.5_AVHRR_simyr1850-2100_c240906.nc.tar.gz
 cd ../../../atm/datm7/NASA_LIS/
 tar -zxvf clmforc.Li_2012_climo1995-2011.T62.lnfm_c130327.nc.tar.gz
 tar -zxvf clmforc.Li_2012_climo1995-2011.T62.lnfm_Total_c140423.nc.tar.gz
